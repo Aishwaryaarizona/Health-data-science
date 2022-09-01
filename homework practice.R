b41e752f3043ce976e2483d35a1e08ca1b81c479
Aishwaryaarizona <aishwaryaacharya@arizona.edu>
2022-09-01 10:29:05 -0700

    Create homework practice.R

---
+++ homework practice.R
@@ -0,0 +1,12 @@
+#get working directory
+getwd()
+
+#install packages
+install.packages("psych")
+install.packages("gmodels")
+install.packages("swirl")
+install.packages("rmarkdown")
+#saves your command automatically in word document
+#after installing packages you need to load it
+load("HOSPITAL.DAT.rdata")
+
